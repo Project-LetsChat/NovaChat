eab55579a885b7784c0b2cd491f166e4881c39da
Bhargav <75622003+BhargavEkbote@users.noreply.github.com>
2024-07-03 08:42:47 +0530

    SECURITY PATCH.
    XSS vulnerability patched in LetsChat_page.js.

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDCKd9OXejZ-0PK729riphOd6Z1KgrzjCo",
+    authDomain: "letschattestbranch.firebaseapp.com",
+    databaseURL: "https://letschattestbranch-default-rtdb.firebaseio.com/",
+    projectId: "letschattestbranch",
+    storageBucket: "letschattestbranch.appspot.com",
+    messagingSenderId: "1048525961569",
+    appId: "1:1048525961569:web:83583f3ab3a726da266d20"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
