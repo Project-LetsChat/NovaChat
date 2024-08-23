ac484350338b53c497a7eb88dcfded09732e93aa
Bhargav <bhargavsdeal@gmail.com>
2024-08-23 10:16:01 +0530

    Realtime Filters Rewrite.
    Added the Realtime Filters Rewrite.

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    
+    canvas = createCanvas(400, 300);
+    canvas.center();
+    
+}
+
+function draw()
+{
+
+}
+
+function save()
+{
+
+    save('my_filter_image.png')
+
+}
